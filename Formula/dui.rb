488ed0a2c5a2cac9ae23e94cc65eb1e421ddac94
ukhan1219 <113568605+ukhan1219@users.noreply.github.com>
2025-08-01 04:16:47 -0400

    brew upload

--- Formula/dui.rb
+++ Formula/dui.rb
@@ -1,8 +1,8 @@
 class Dui < Formula
   desc "An intuitive Docker management CLI built in Rust"
   homepage "https://github.com/ukhan1219/dui"
-  url "https://github.com/ukhan1219/dui/archive/refs/tags/v3.6.0.tar.gz"
-  sha256 "PLACEHOLDER_SHA256" # This will be updated with actual SHA256
+  url "https://github.com/ukhan1219/dui/archive/refs/tags/v4.0.10.tar.gz"
+  sha256 "eaf450e7ffe00f0e81482e9ec5c23afc4aa15ffeac2aa81acc837ac2a7e89e80"
   license "MIT"
   head "https://github.com/ukhan1219/dui.git", branch: "main"
 
